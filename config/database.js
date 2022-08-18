ecf042f11d4c9ba55d92f40a3cb72b3981eb0ad6
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2022-08-18 22:03:18 +0700

    Commit ke SQL Branch

--- config/database.js
+++ config/database.js
@@ -2,18 +2,13 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.mvsz4.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', 't9d18svc4LGJmWcG'),
+        client: 'sqlite',
+        filename: '.tmp/data.db',
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        useNullAsDefault: true,
       },
     },
   },
